2f1576b24c8a4eb0804c2b272d3c46e95515ad8e
Andre Williams <awjrs22@gmail.com>
2020-02-15 13:59:16 -0800

    initial deploy

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+from send_mail import send_mail
 app = Flask(__name__)
 
 # When we switch to Heroku we'll have a totally different database
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug=True
@@ -12,7 +12,7 @@ if ENV == 'dev':
 else:
     app.debug=False
     # Our production database
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tzpoyxfmbqoany:a5c5fbb1e9b7965541078b2fd3adb738ab6948d67b7ef4ea2cce4eebfdecff6c@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d4k79366nf75pv'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -50,6 +50,7 @@ def submit():
             data = Feedback(customer, dealer, rating, comments)
             db.session.add(data)
             db.session.commit() # Adds customer to the database
+            send_mail(customer, dealer, rating, comments)
 
             return render_template('success.html')
         return render_template('index.html', message='You have already submitted feedback!!!')
--- send_mail.py
+++ send_mail.py
@@ -6,3 +6,16 @@ def send_mail(customer, dealer, rating, comments):
     smtp_server = 'smtp.mailtrap.io'
     login = '9d0e3cddc021a1'
     password = 'ca1d8f7f0e93fd'
+    message = f'<h2>New Feedback Submission</h2><ul> <li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Dealer: {rating}</li><li>Dealer: {comments}</li> </ul>'
+
+    sender_email = 'email1@example.com'
+    receiver_email = 'email2@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexuas Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email 
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
